219651b7bb3dd0cf6a3e2a99dcbae476b821a50a
Nix Tay <niiiiiiiiiiix@users.noreply.github.com>
2021-02-01 14:51:34 +0800

    Displaying Todo items

---
+++ src/components/Todolist.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        { name: "Buy Milk", isDone: false },
+        { name: "Do push up", isDone: true },
+      ],
+    };
+  }
+
+  displayTodos() {
+    return this.state.todos.map((todo) => (
+      <div>
+        <span>{todo.name}</span>
+        <span>{todo.isDone ? " - completed" : " - not completed"}</span>
+      </div>
+    ));
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Todolist</div>
+        <div>{this.displayTodos()}</div>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
